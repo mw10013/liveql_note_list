b5e2ab4ee0d6117d2e615ae2c887f7c13fa6903c
Michael Wu <mw10013@gmail.com>
2021-08-26 12:42:59 -0400

    input section wip

--- src/App.js
+++ src/App.js
@@ -634,6 +634,11 @@ function InputSection({ insertNotes }) {
             {...getFieldProps("duration")}
           />
           <InputField id="step" label="Step" {...getFieldProps("step")} />
+          <ButtonGroup
+            left={{ onClick: insert, children: "Insert" }}
+            middle={{ onClick: insertAndStep, children: "Insert+Step" }}
+            right={{ onClick: step, children: "Step" }}
+          />
           <div className="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
             <label
               htmlFor="pitch"
