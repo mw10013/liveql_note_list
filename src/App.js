1d97d6fbd452baff720f9f16a9dee6677a338eb1
Michael Wu <mw10013@gmail.com>
2021-09-04 14:08:53 -0400

    pagination example

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { useTable, usePagination, useRowSelect } from "react-table";
 import styled from "styled-components";
 import update from "immutability-helper";
 
-// TODO: disclosure box, table; dupes, pagination, disabled button
+// TODO: disclosure box, table; dupes, pagination
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -312,6 +312,37 @@ const defaultColumn = {
   Cell: EditableCell,
 };
 
+function PaginationExample() {
+  return (
+    <nav
+      className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+      aria-label="Pagination"
+    >
+      <div className="hidden sm:block">
+        <p className="text-sm text-gray-700">
+          Showing <span className="font-medium">1</span> to{" "}
+          <span className="font-medium">10</span> of{" "}
+          <span className="font-medium">20</span> results
+        </p>
+      </div>
+      <div className="flex-1 flex justify-between sm:justify-end">
+        <a
+          href="#"
+          className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+        >
+          Previous
+        </a>
+        <a
+          href="#"
+          className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+        >
+          Next
+        </a>
+      </div>
+    </nav>
+  );
+}
+
 function Table({ columns, data, updateNote, skipPageReset, setSelection }) {
   const {
     getTableProps,
@@ -397,6 +428,7 @@ function Table({ columns, data, updateNote, skipPageReset, setSelection }) {
           </div>
         </div>
       </div>
+      <PaginationExample />
     </div>
   );
 }
